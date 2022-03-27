5b6a6e1447f868a0efd5333997a076a05491c21c
Muaz <96747337+MuazV@users.noreply.github.com>
2022-03-27 08:16:17 +0300

    Update Persistent Bugger.py

--- Persistent Bugger.py
+++ Persistent Bugger.py
@@ -7,28 +7,6 @@
 #     for ii in new_list:
 #         sonuc *= int(ii)
 # print(sonuc)
-    
-        
-
-
-# def multiply(muaz):
-#     result = 1
-#     for i in muaz:
-#         result *= i
-#     return result
-
-# multiply(list)
-
-# def persistence(num):
-#     if num < 10:
-#         return num # Only one digit. Can't iterate over it
-#     num_str = str(num)
-#     total = 1
-#     for i in num_str:
-#         total = total * int(i)
-#     return 1 + persistence(total)
-
-# print(persistence(1234566))
 
 def persistance(num):
     counter=0
