8e48f1b9f1c13c7109a7390be6c55d60e89c23e7
firatolcum <firatolcum@anadolu.edu.tr>
2022-03-29 16:10:42 +0300

    a docstring add in anagram.py about anagram's definition.

--- anagram.py
+++ anagram.py
@@ -1,8 +1,14 @@
+"""
+Definition of Anagram : a word or phrase made by transposing the letters of another word or phrase.
+                   Ex : The word "secure" is an anagram of "rescue."
+"""
+
 # anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
 
 # anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
 
 # anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+
 
 def anagrams(word, words):
     anagram_list = []
@@ -11,40 +17,35 @@ def anagrams(word, words):
             anagram_list.append(i)
     return anagram_list
 
+
 print(anagrams('laser', ['lazing', 'lazy',  'lacer']))
 print(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']))
 print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))
-
-
-
 
 
 # word_list = ["percussion", "supersonic", "car", "tree", "boy", "girl", "arc"]
 
 # # initialize a list
 # anagram_list = []
-# for word_1 in word_list: 
-#     for word_2 in word_list: 
+# for word_1 in word_list:
+#     for word_2 in word_list:
 #         if word_1 != word_2 and (sorted(word_1)==sorted(word_2)):
 #             anagram_list.append(word_1)
 # print(anagram_list)
 
 
+# words = ["eat", "tea", "tan", "ate", "nat", "bat"]
+# anagrams = {}
 
-
-# words = ["eat", "tea", "tan", "ate", "nat", "bat"]  
-# anagrams = {}                                            
-
-# for i in words:       
+# for i in words:
 #    ii = str(sorted(i))
 #    if ii in anagrams:
 #       anagrams[ii].append(i)
 #    else:
 #       anagrams[ii] = [i]
-      
+
 # liste= list(anagrams.values())
 # print(liste)
-
 
 
 # anagram = ["Muaz", "zam", "zamu", "azam"]
