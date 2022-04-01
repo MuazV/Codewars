a109ec9e3602c7fea17c74763ef3cc05fc3a7c91
muaz <muaz.vakitsayan@gmail.com>
2022-04-01 14:07:22 +0300

    Mumbling and A chain addind Function Q&A added

---
+++ Mumbling.py
@@ -0,0 +1,23 @@
+# This time no story, no theory. The examples below show you how to write function accum:
+
+# Examples:
+# accum("abcd") -> "A-Bb-Ccc-Dddd"
+# accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+# accum("cwAt") -> "C-Ww-Aaa-Tttt"
+# The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+
+# Solution-1
+# def accum(s):
+#   return "-".join([(s[i]*(i+1)).title() for i in range(len(s))])
+
+#Solution-2
+def accum(item):
+    liste = []
+    for i in range(len(item)):
+        liste.append((item[i]*(i+1)).title())
+        
+    result = '-'.join(liste)
+    return result
+        
+accum('item')
